cade1390e8bc7bbcd9d8598a6231a5378daa7861
hwh-DLFan <152786945+hwh-DLFan@users.noreply.github.com>
2024-01-06 10:20:05 +0800

    Update rational.h
    Signed-off-by: hwh-DLFan <152786945+hwh-DLFan@users.noreply.github.com>

--- C++ Extra Header/rational.h
+++ C++ Extra Header/rational.h
@@ -3,9 +3,12 @@ Copyright (c) 2024 hwh-DLFan@github.com
 This file provides rational number.
 #endif
 
+#ifdef __cplusplus
 #ifndef _RATIONAL_H
 #define _RATIONAL_H 1
+#ifndef _rational
+#define _rational 202401L
 
-
-
-#endif //_RATIONAL_H
+#endif
+#endif
+#endif
